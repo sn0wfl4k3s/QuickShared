9ceddf8dcc8c435821c274fe65e14960c2a62e43
Edu <ed.afram@gmail.com>
2025-09-15 09:29:58 -0300

    changes

---
+++ src/QuickShared.Web/Pages/Download.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace QuickShared.Web.Pages
+{
+    public class DownloadModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
